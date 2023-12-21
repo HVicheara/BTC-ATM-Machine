c48ec871085dcf6c6415db4c079ddb1d2d2765fe
Pisoth Yi <pisothyi@gmail.com>
2023-12-21 10:47:15 +0700

    Add banking functions to Customer Class

--- Customer.java
+++ Customer.java
@@ -1,4 +1,5 @@
 public class Customer extends User {
+    private double btcUnitPrice = 43593.50;
     // Full Constructor
     public Customer(String userID, String fullName, String phoneNumber, String otp, double balance) {
         this.userID = userID;
@@ -19,6 +20,24 @@ public class Customer extends User {
     public Customer(String phoneNumber, String otp) {
         this.phoneNumber = phoneNumber;
         this.otp = otp;
+    }
+
+    public void withDraw(double btcAmount){
+        if(this.balance > btcAmount){
+            balance -= btcAmount;
+            System.out.println("Customer withdrawn: " + btcAmount + "successfully");
+            System.out.println(btcAmount + " BTC = " + btcAmount * btcUnitPrice);
+        }else{
+            System.out.println("Insufficient fund.");
+        }
+    }
+
+    public void deposit(double usdAmount){
+        if(usdAmount > 0.0){
+            balance += usdAmount / btcUnitPrice;
+        }else{
+            System.out.println("Please deposit fund.");
+        }
     }
 
     @Override
